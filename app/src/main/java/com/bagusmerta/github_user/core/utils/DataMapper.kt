dc87c9fa5453f63f000fd5d8ac27af7cc4c263f1
Bagus Merta <merta.bagus24@gmail.com>
2022-01-03 14:25:22 +0800

    [#2] add data mapper for detail response

--- app/src/main/java/com/bagusmerta/github_user/core/utils/DataMapper.kt
+++ app/src/main/java/com/bagusmerta/github_user/core/utils/DataMapper.kt
@@ -1,6 +1,8 @@
 package com.bagusmerta.github_user.core.utils
 
+import com.bagusmerta.github_user.core.data.source.remote.response.UserDetailResponse
 import com.bagusmerta.github_user.core.data.source.remote.response.UserItemResponse
+import com.bagusmerta.github_user.core.domain.model.UserDetail
 import com.bagusmerta.github_user.core.domain.model.UsersItemSearch
 
 object DataMapper {
@@ -13,5 +15,18 @@ object DataMapper {
                 avatar = it.avatar,
             )
         }
+
+    fun mapUserDetailResponseToDomain(data: UserDetailResponse): UserDetail =
+        UserDetail(
+            avatarUrl = data.avatarUrl.toString(),
+            name = data.name.toString(),
+            login = data.login.toString(),
+            followers = data.followers,
+            following = data.following,
+            publicRepos = data.publicRepos,
+            id = data.id,
+            location = data.location,
+            company = data.company
+        )
 
 }
