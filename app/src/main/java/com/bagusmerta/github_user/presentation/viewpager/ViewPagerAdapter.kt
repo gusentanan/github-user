5f179b5fa1f6e7f41aeae79d7e5849b6cb05cf65
Bagus Merta <merta.bagus24@gmail.com>
2022-01-04 14:12:39 +0800

    [#2] initialize fragment for viewpager

--- app/src/main/java/com/bagusmerta/github_user/presentation/viewpager/ViewPagerAdapter.kt
+++ app/src/main/java/com/bagusmerta/github_user/presentation/viewpager/ViewPagerAdapter.kt
@@ -1,4 +1,19 @@
 package com.bagusmerta.github_user.presentation.viewpager
 
-class ViewPagerAdapter {
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.bagusmerta.github_user.core.domain.model.UsersItemSearch
+
+class ViewPagerAdapter(
+    activity: AppCompatActivity,
+): FragmentStateAdapter(activity){
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+
 }
