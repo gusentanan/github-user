f3ff1b95a9e42dc4a4c0146fe07cfc928e9fba39
Bagus Merta <merta.bagus24@gmail.com>
2022-01-04 14:25:55 +0800

    [#2] add services and model for followers

--- app/src/main/java/com/bagusmerta/github_user/core/data/source/remote/network/ApiServices.kt
+++ app/src/main/java/com/bagusmerta/github_user/core/data/source/remote/network/ApiServices.kt
@@ -2,6 +2,8 @@ package com.bagusmerta.github_user.core.data.source.remote.network
 
 import com.bagusmerta.github_user.core.data.source.remote.response.SearchResponse
 import com.bagusmerta.github_user.core.data.source.remote.response.UserDetailResponse
+import com.bagusmerta.github_user.core.data.source.remote.response.UsersFollowingResponse
+import com.bagusmerta.github_user.core.domain.model.UsersItemSearch
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,5 +15,11 @@ interface ApiServices {
 
     @GET("users/{username}")
     suspend fun getDetailUser(@Path("username") username: String): UserDetailResponse
+
+    @GET("users/{username}/followers")
+    suspend fun getUserFollowers(@Path("username") username: String): UsersFollowingResponse
+
+    @GET("users/{username}/following")
+    suspend fun getUserFollowing(@Path("username") username: String): UsersFollowingResponse
 
 }
